df001b5880cc66ea7d80d5413fddf748fe7cd4e3
Naga Sai Ajay Kumar <86917697+kumar57205@users.noreply.github.com>
2022-01-22 19:54:02 +0530

    Create DistanceConvertorM.java

---
+++ DistanceConvertorM.java
@@ -0,0 +1,17 @@
+package DistanceConvertor;
+
+public class DistanceConvertorM {
+	public double MetertoKm(double meter) {
+		return meter/1000;
+	}
+	public double KmToMeter(double km) {
+		return km*1000;
+	}
+	public double MilesToKm(double miles) {
+		return miles*1.609;
+	}
+	public double KmToMiles(double km) {
+		return km/1.609;
+	}
+
+}
