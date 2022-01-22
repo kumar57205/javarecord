bc5c7584d6b98ae44305bb91969fb1042d2b7cb3
Naga Sai Ajay Kumar <86917697+kumar57205@users.noreply.github.com>
2022-01-22 19:53:15 +0530

    Create CurrencyConvertorM.java

---
+++ CurrencyConvertorM.java
@@ -0,0 +1,22 @@
+package Currencyconvertor;
+
+public class CurrencyConvertorM {
+	public double DollarIndian(double dollar) {
+		return dollar*74.36;
+	}
+	public double IndianDollar(double ruppes) {
+		return ruppes/74.36;
+	}
+	public	double EuroIndian(double euro) {
+		return euro*84.57;
+	}
+	public double IndianEuro(double ruppees) {
+		return ruppees/84.57;
+	}
+	public double YentoIndian(double yen) {
+		return yen*0.65;
+	}
+	public double IndiantoYen(double rupees) {
+		return rupees/0.65;
+	}
+}
